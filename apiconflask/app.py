114feb3ff45dafff3119b921251bea4d96c74fe1
Juan Camilo Molina Diaz <55802960+JuanCamiloMD96@users.noreply.github.com>
2022-05-28 20:37:29 -0500

    Add files via upload

---
+++ apiconflask/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import requests
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    datosobtenidos = requests.get('https://api.dailymotion.com/videos?channel=sport&limit=10')
+    datosenfomatoJSON = datosobtenidos.json()
+    return render_template('index.html', datos=datosenfomatoJSON['list'])
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
+ 
